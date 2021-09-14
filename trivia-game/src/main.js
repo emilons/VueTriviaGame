1c78640ed12b92a0b9852ecac1cc0c8429b90ffb
Edlix <56119942+Edlix@users.noreply.github.com>
2021-09-14 15:00:10 +0200

    Added routing

--- trivia-game/src/main.js
+++ trivia-game/src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import StartScreen from './Questionaire'
+//import StartScreen from './Questionaire'
+import router from "./router"
+//import store from "./store"
 
 import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
@@ -11,6 +13,7 @@ Vue.use(BootstrapVue);
 Vue.config.productionTip = false
 
 new Vue({
-  StartScreen,
+  router,
+  //StartScreen,
   render: h => h(App),
 }).$mount('#app')
---
+++ trivia-game/src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: "/home",
+        name: "Home",
+        component: () => import("../components/Home/HomePage.vue")
+    },
+    {
+        path: "/questions",
+        name: "QuestionPage",
+        component: () => import("../components/Question/QuestionPage.vue")
+    },
+    {
+        path: "/results",
+        name: "Results",
+        component: () => import("../components/Result/ResultPage.vue")
+    }
+]
+
+const router = new VueRouter({
+
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes
+})
+
+export default router;
