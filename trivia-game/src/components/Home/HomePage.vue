b4e4bf2fbf91d29d4450997ac857f75da1482e9d
Edlix <56119942+Edlix@users.noreply.github.com>
2021-09-15 14:21:58 +0200

    Pushing for sync

--- trivia-game/src/components/Home/HomePage.vue
+++ trivia-game/src/components/Home/HomePage.vue
@@ -1,21 +1,55 @@
 <template>
-  <div class="container">
+     <div class="container">
     <label>Username:</label>
     <input type="text" />
     <MenuOption/>
-  </div>
+      
+
+  <div class="container">
+    <label>Username:</label>
+    <input type="text" />
+    <b-button variant="primary" @click="onClickGoToQuestions">Start Game</b-button>
+    <MenuOption/>
+
+   
+  </div>   
 </template>
 
 <script>
 
+
 import MenuOption from './MenuOption.vue';
+
+import{mapActions, mapMutations, mapState} from 'vuex'
 
 export default {
   name: 'HomePage',
-  components: {
+    components: {
     MenuOption
+    },
+
+  computed: {
+    ...mapState(["error"])
+
+  },
+  methods: {
+    ...mapActions(["loginUser"]),
+    ...mapMutations(["setUsername", "setHighscore"]),
+    onClickGoToQuestions(){
+      this.loginUser()
+      if (!this.error){
+        this.$router.push("/questions")
+      }
+
+    },
+    onUserNameChange(event){
+
+
+    },
+
   }
-}
+} 
+
 
 </script>
 
--- trivia-game/src/main.js
+++ trivia-game/src/main.js
@@ -3,12 +3,18 @@ import App from './App.vue'
 import router from "./router"
 import Store from './store/index.js'
 
+
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 import 'bootstrap/dist/css/bootstrap.css';
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   Store,
+
   render: h => h(App),
 }).$mount('#app')
