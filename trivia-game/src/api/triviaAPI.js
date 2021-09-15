d32ea56b096f242f93f9333701fe9fc2f9f8b368
emilons <emilons@hotmail.com>
2021-09-15 16:22:14 +0200

    added getTriviaGame function

--- trivia-game/src/api/triviaAPI.js
+++ trivia-game/src/api/triviaAPI.js
@@ -11,11 +11,15 @@ export const getTriviaCategories = async () => {
     }
 }
 
-export const getTriviaGame = async () => {
+export const getTriviaGame = async (amount, category, difficulty) => {
     try {
-        //
+        const gameUrl = 'https://opentdb.com/api.php?' + `amount=${amount}` + `&category=${category.id}` + `&difficulty=${difficulty}`;
+        const {results} = await fetch(gameUrl)
+            .then(r => r.json())
+        return [null, results];
+
     }
     catch (e) {
-        //
+        return [e.message, null];
     }
 }
